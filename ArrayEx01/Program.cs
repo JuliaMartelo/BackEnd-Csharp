dbad5535b1491923dad33db3a198321fe3c3547e
Julia Martelo <JuliaMartelo2007@gmail.comgit comfig --listgit config --global user.email JuliaMartelo2007@gmail.com>
2024-10-24 16:50:34 -0300

    atualizando

---
+++ ArrayEx01/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numeros = new int [6];
+int numeroImpar;
+
+for (int i = 0; i < numeros.Leight; i++)
+{
+    Console.WriteLine($"Digite um numero inteiro: "!);
+    numeros[1] = int.Parse(Console.ReadLine()!);
+
+    if(numeros [1] %2 ==0)
+    {
+        numeroPar++;
+    }
+
+    else{
+        numeroImpar++;
+    }
+}
+
+Console.WriteLine($"Quantidade de numeros pares: "!);
+Console.WriteLine($"Quantidade de numeros impares: "!);
---
+++ Program/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] carros = new string [3];
+
+for (carros = 0; i < carros.Leight; i ++)
+{
+    Console.WriteLine($"Digite o nome do carro a ser cadastrado: ");
+    carros [i] = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Os carros cadastrados são: {carros [0]}, {carros [1]}, {carros [2]}");
+
